717ceb1cb0373f47fc54cdaf0b2d556cad8cf5d3
thangkiller <ngenvan000@gmail.com>
2022-07-28 07:45:08 +0700

    add button and primary navigation navbar in header

--- react/bonsai/src/component/Layout/component/Header.js
+++ react/bonsai/src/component/Layout/component/Header.js
@@ -1,11 +1,34 @@
-import styles from "./Header.module.scss";
+import { Link } from 'react-router-dom';
+import Button from '~/component/Button';
+import { Logo } from '~/asset/images';
+import styles from './Header.module.scss';
 
 const $ = styles;
 
 function Header() {
   return (
     <div className={$.wrapper}>
-      <div className={$.inner}>navbar</div>
+      <div className={$.inner}>
+        <div className={$.home}>
+          <Link to="/">
+            <Logo />
+          </Link>
+        </div>
+        <div className={$.navigation}>
+          <div className={$.left}>
+            <div>Product</div>
+            <div>Templates</div>
+            <Link to="/pricing">Pricing</Link>
+            <div>Reviews</div>
+          </div>
+          <div className={$.right}>
+            <Button white small>
+              log in
+            </Button>
+            <Button>start free</Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
