51f765e1591d71968e220925b3ac9449c0356644
ngenvan000@gmail.com <ngenvan000@gmail.com>
2022-08-06 19:32:23 +0700

    data content #1

--- react/bonsai/src/component/pages/Home/index.js
+++ react/bonsai/src/component/pages/Home/index.js
@@ -1,5 +1,14 @@
 function Home() {
-  return <div>Home page</div>;
+  return <div>
+    <div className={$.email}>
+      <h1>Everything you need to run your business.</h1>
+      <p>Bonsai's all-in-one product suite with smart automation lets you focus on your passion, not your paperwork.</p>
+      <form>
+        <input placeholder='Enter your email' />
+          <Button>start here</Button>
+      </form>
+    </div>
+  </div>;
 }
 
 export default Home;
