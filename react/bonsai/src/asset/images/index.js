df2e4e717412c8626eb2ffaede4bc390df70f271
thangkiller <ngenvan000@gmail.com>
2022-08-14 21:55:26 +0700

    add pricing #1

--- react/bonsai/src/asset/images/index.js
+++ react/bonsai/src/asset/images/index.js
@@ -4,3 +4,5 @@ export { default as Without__Bonsai } from './Without__Bonsai.svg';
 export { default as With__Bonsai } from './With__Bonsai.svg';
 export { default as network_1 } from './network_1.svg';
 export { default as network_2 } from './network_2.svg';
+export { default as tick } from './tick__image.svg';
+export { default as endow } from './endow.svg';
--- react/bonsai/src/router/index.js
+++ react/bonsai/src/router/index.js
@@ -1,10 +1,10 @@
-import { DefaultLayout, HeaderOnly } from "~/component/Layout";
-import { Home, Pricing, Seclude } from "~/component/pages";
+import { DefaultLayout, HeaderOnly } from '~/component/Layout';
+import { Home, Pricing, Seclude } from '~/component/pages';
 
 const publicRouters = [
-  { path: "/", element: Home, layout: DefaultLayout },
-  { path: "/pricing", element: Pricing, layout: HeaderOnly },
-  { path: "/seclude", element: Seclude, layout: null },
+  { path: '/', element: Home, layout: DefaultLayout },
+  { path: '/pricing', element: Pricing, layout: DefaultLayout },
+  { path: '/seclude', element: Seclude, layout: HeaderOnly },
 ];
 
 const privateRouters = [];
